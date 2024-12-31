b58e654dcda7d325c0937bf11f3096f0016270f7
shubhamsingh91 <shubham.singh091@gmail.com>
2024-12-30 18:46:56 -0600

    corrected S_ring

--- sphericalZYX/test_spherical_derivs.m
+++ sphericalZYX/test_spherical_derivs.m
@@ -16,20 +16,10 @@ s_ring_jac = jacobian(S*v,q); % not correct
 
 for i = 1:3
     temp1 = jacobian(S(:,i),q); % this tensor is d(S_i)/dq , each page is d(S_i)/dq
-    s_ring_v2(:,i) = temp1*v;% this is d(S_i)/dq  * v
+    s_ring(:,i) = temp1*v;% this is d(S_i)/dq  * v
 end
 
-s_temp = s_ring(q,v);
-
-diff = simplify(s_temp - s_ring_v2)
-
-% for i = 1:3
-%     temp1 = diff(S,q(i)); % this tensor is d(S)/dq_i 
-%     s_ring_v3(:,i) = temp1*v;% this is d(S_i)/dq  * v
-% end
-
-
-matlabFunction(s_ring_v2, 'Vars', {q, v}, 'File', 's_ring', 'Outputs', {'s_ring'});
+% matlabFunction(s_ring, 'Vars', {q, v}, 'File', 's_ring', 'Outputs', {'s_ring'});
 % 
 % partial (S_ring * v)/partial q
 % s_ring_times_v_partial = jacobian(s_ring_v2*v,q);
