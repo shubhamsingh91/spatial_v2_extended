7e6c333b4caf35c8840e1378cfa988f88968d33f
shubhamsingh91 <shubham.singh091@gmail.com>
2024-12-31 11:08:36 -0600

    corrected functions

--- sphericalZYX/test_spherical_derivs.m
+++ sphericalZYX/test_spherical_derivs.m
@@ -12,17 +12,47 @@ S = sphericalZYXSubspace(q);
 
 % S_ring = partial (S)/partial (q)  * v
 syms v [3,1 ] real
-s_ring_jac = jacobian(S*v,q); % not correct
+syms f [6,1 ] real
+syms m [3,1 ] real
+dSm_sq = jacobian(S*m,q); 
+
+%% S_ring
 
 for i = 1:3
     temp1 = jacobian(S(:,i),q); % this tensor is d(S_i)/dq , each page is d(S_i)/dq
     s_ring(:,i) = temp1*v;% this is d(S_i)/dq  * v
 end
 
-% matlabFunction(s_ring, 'Vars', {q, v}, 'File', 's_ring', 'Outputs', {'s_ring'});
-% 
-% partial (S_ring * v)/partial q
-% s_ring_times_v_partial = jacobian(s_ring_v2*v,q);
-% matlabFunction(s_ring_times_v_partial, 'Vars', {q, v}, 'File', 's_ring_v_deriv', 'Outputs', {'s_ring_v_partial'});
+% Pat's version
+s_ring_v2 = sym(zeros(6,3));
+for i = 1:3
+    ds_dq(:,:,i) = diff(S,q(i)); % this tensor is d(S_i)/dq , each page is d(S_i)/dq
+    s_ring_v2  =s_ring_v2+ ds_dq(:,:,i)*v(i);% this is d(S_i)/dq  * v
+end
+
+s_ring_v3 = jacobian(jacobian(S*m,q)*v,m);
+
+disp(s_ring - s_ring_v2)
+disp(s_ring - s_ring_v3)
+
+%sanity check
+disp("sanity check = ")
+disp(jacobian(s_ring*v,v) - (s_ring + jacobian(S*v,q)))
+
+matlabFunction(s_ring, 'Vars', {q, v}, 'File', 's_ring', 'Outputs', {'s_ring'});
+%
+
+%% d/dq Sm, d/dq S'T * f
+
+dSm_dq = jacobian(S*m,q);
+matlabFunction(dSm_dq, 'Vars', {q, m}, 'File', 'dSm_dq', 'Outputs', {'dSm_dq'});
+
+dSTf_dq = jacobian(S.'*f,q);
+matlabFunction(dSTf_dq, 'Vars', {q, f}, 'File', 'dSTf_dq', 'Outputs', {'dSTf_dq'});
+
+%% partial(S_ring * v)/partial q
+
+s_ring_v_deriv = jacobian(s_ring*v,q);
+matlabFunction(s_ring_v_deriv, 'Vars', {q, v}, 'File', 's_ring_v_deriv', 'Outputs', {'s_ring_v_deriv'});
 
 
